aeb8103018b2abc1ac89f1474ea285747d350ec5
unknown <mustufabaigwastaken@gmail.com>
2026-03-04 15:47:54 +0530

    solved Insert Intervals

---
+++ 6 Intervals/3 Insert Intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
+        merged=[intervals[0]]
+        
+        i=1
+        while i<len(intervals):
+            if merged[-1][1]>=intervals[i][0]:
+                merged[-1][1]=max(intervals[i][1],merged[-1][1])
+            else:
+                merged.append(intervals[i])
+            i+=1
+
+        return merged
+        
