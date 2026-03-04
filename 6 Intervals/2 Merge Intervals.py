5f742e71bb331ab32c0487bda52694d7db06bb21
unknown <mustufabaigwastaken@gmail.com>
2026-03-04 13:43:51 +0530

    solved Merge Intervals

---
+++ 6 Intervals/2 Merge Intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged=[intervals[0]]
+        
+        i=1
+        while i<len(intervals):
+            if merged[-1][1]>=intervals[i][0]:
+                merged[-1][1]=max(intervals[i][1],merged[-1][1])
+            else:
+                merged.append(intervals[i])
+            i+=1
+
+        return merged
+        
