5b7e92be81158ba00a991ff937ff0425be6e293e
unknown <mustufabaigwastaken@gmail.com>
2026-04-16 22:03:33 +0530

    solved unique-paths-ii

---
+++ 23 Multidimensional DP/unique-paths-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def uniquePathsWithObstacles(self, grid):
+        m, n = len(grid), len(grid[0])
+
+        if grid[0][0] == 1:
+            return 0
+
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+
+        for j in range(1, n):
+            if grid[0][j] == 1:
+                dp[0][j] = 0
+            else:
+                dp[0][j] = dp[0][j - 1]
+
+        for i in range(1, m):
+            if grid[i][0] == 1:
+                dp[i][0] = 0
+            else:
+                dp[i][0] = dp[i - 1][0]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
